0b9b85ff847d23eb0fdc54770ccec42f12f4c845
MichailVs <mcohotnik21@gmail.com>
2024-03-06 00:03:29 +0300

    Переделал гамбургер меню

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const view = document.querySelector('.view')
 const watch = document.querySelector('.watch')
 const popup = document.querySelector('.popup')
 const menuBtn = document.querySelector('.menu')
-const header = document.querySelector('.header')
 const body = document.body
 
-pages.addEventListener('click', e => {
+// Navbar
+pages.addEventListener('click', e => nav(e, pageList))
+function nav(e, pageList) {
     e.preventDefault()
     if (e.target.classList.contains('navbar-item')) {
-        clearClass(e.target.innerText)
+        clearClass(e.target.innerText, pageList)
         e.target.classList.contains('page-active') ? 0 : e.target.classList.toggle('page-active')
     }
-})
-
-function clearClass(text) {
+}
+function clearClass(text, pageList) {
     for (let el of pageList) {
         if (el.innerText !== text && el.classList.contains('page-active')) {
             el.classList.toggle('page-active')
@@ -24,6 +24,8 @@ function clearClass(text) {
     }
 }
 
+
+// Buttons on main page
 view.addEventListener('click', btnAction)
 watch.addEventListener('click', btnAction)
 function btnAction() {
@@ -32,23 +34,7 @@ function btnAction() {
     this.classList.contains('btn-active') ? 0 : this.classList.toggle('btn-active')
 }
 
-
-// header.addEventListener('click', e => {
-//     if (e.target.classList.contains('bar') && popup.classList.contains('open')) {
-//         body.classList.toggle('noscroll')
-//         menuBtn.classList.toggle('menu__actove')
-//         popup.classList.toggle('open')
-//         popup.removeChild(navbar)
-//         return
-//     }
-//     if (e.target.classList.contains('bar') && !popup.classList.contains('open')) {
-//         body.classList.toggle('noscroll')
-//         menuBtn.classList.toggle('menu__actove')
-//         popup.classList.toggle('open')
-//         popup.appendChild(navbar)
-//         return
-//     }
-// })
+// Hamburger menu
 menuBtn.addEventListener('click', () => {
     if (popup.classList.contains('open')) {
         body.classList.toggle('noscroll')
@@ -61,6 +47,8 @@ menuBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('menu__actove')
         popup.classList.toggle('open')
         popup.appendChild(navbar)
+        const navItems = navbar.querySelectorAll('li')
+        navbar.addEventListener('click', e => nav(e, navItems))
         return
     }
 })
